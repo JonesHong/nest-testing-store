34b8870f51cf7946ba6cde8ad51ead4046a85ee3
Joneshong <latte831104@gmail.com>
2022-09-16 12:36:17 +0800

    🦄 refactor(setAppModule):

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import "reflect-metadata"
+import { Cqrs } from '@/mycena-store/mycena-store.unittest';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  Cqrs.setAppModule(AppModule, app);
 }
 bootstrap();
